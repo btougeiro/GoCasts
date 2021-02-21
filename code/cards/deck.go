64ed8cc62c871d4df43f79f2b6f6f0382e71a58d
Bruno Tougeiro <b.tougeiro@icloud.com>
2021-02-21 23:02:01 +0000

    added all cards - created a simple approach on shuffle function

--- code/cards/deck.go
+++ code/cards/deck.go
@@ -17,7 +17,21 @@ func newDeck() deck {
 	cards := deck{}
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four"}
+	cardValues := []string{
+		"Ace",
+		"Two",
+		"Three",
+		"Four",
+		"Five",
+		"Six",
+		"Seven",
+		"Eight",
+		"Nine",
+		"Ten",
+		"Jack",
+		"Queen",
+		"King",
+	}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
@@ -60,12 +74,7 @@ func newDeckFromFile(filename string) deck {
 }
 
 func (d deck) shuffle() {
-	source := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(source)
+	rand.Seed(time.Now().UnixNano())
 
-	for i := range d {
-		newPosition := r.Intn(len(d) - 1)
-
-		d[i], d[newPosition] = d[newPosition], d[i]
-	}
+	rand.Shuffle(len(d), func(i, j int) { d[i], d[j] = d[j], d[i] })
 }
